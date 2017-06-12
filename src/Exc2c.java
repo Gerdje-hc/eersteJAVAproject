642a0396f9744025bd3722e203e745dfa7a63fc3
Gerdje-hc <gerdje.hc@gmail.com>
2017-06-13 00:10:21 +0200

    thuis gemaakte oefeningen

---
+++ src/Exc2c.java
@@ -0,0 +1,30 @@
+/**
+ * Created by gerdhung-chu on 12/06/17.
+ */
+import java.util.Scanner;
+public class Exc2c {
+    public static void main (String[]args) {
+        int getal1, getal2, getal3, gemiddelde;
+        /** 1) ingeven van de variabelen - kan overal als je ze nodig hebt) */
+        Scanner in =  new Scanner (System.in);
+        /** Leest in wat de gebruiker ingeeft in het keybord */
+        System.out.println(" Geef getal 1");
+        /** Print de vraag om iets in te geven naar het scherm */
+        getal1 = in.nextInt();
+        /** Bepaalt de naam en het type van de variabele waar de input moet in worden opgeslagen */
+        System.out.println(" Geef getal 2");
+        getal2 = in.nextInt();
+        System.out.println(" Geef getal 3");
+        getal3 = in.nextInt();
+
+        gemiddelde = (getal1+getal2+getal3)/3;
+        System.out.printf (" Het gemiddelde van uw 3 getallen is ", gemiddelde);
+        System.out.println();
+
+
+
+
+
+
+    }
+}
--- src/exercise2c.java
+++ src/exercise2c.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
-
+import java.util.StringJoiner;
 public class exercise2c{
     public static void main(String[]args){
-        Scanner s = new Scanner(System.in);
-        int getal1;
-        int getal2;
-        int getal3;
+
+        Scanner scan1 = new Scanner(System.in);
+        String getal1 = scan1.next();
+        Scanner scan2 = new Scanner (System.in);
+        String getal2 = scan2.next();
+        Scanner scan3 = new Scanner(System.in);
+        String getal3 = scan3.next();
+        System.out.println ("Geeft 3 getallen");
+
+
 
     }
 }
