478034ae17e7c559f7c61640953d1c5c9d457920
Gerdje Hung-chu <gerdje.hc@gmail.com>
2017-06-23 15:50:14 +0200

    Update vanuit les 23/06

---
+++ src/be/mijn_oefeningen/Oef_Pojo/RunnerMetTryCatch.java
@@ -0,0 +1,30 @@
+package be.mijn_oefeningen.Oef_Pojo;
+
+import jdk.internal.org.objectweb.asm.tree.TryCatchBlockNode;
+
+/**
+ * Created by vdabcursist on 23/06/2017.
+ */
+
+public class RunnerMetTryCatch {
+
+
+    public static void main(String[] args) {
+
+        try {
+
+            Dog[] dogAsiel = new Dog[3];   /* new ARRAY - Of we geven een hoeveelheid elementen in ts [] of je somt alle
+                                          elementen op , verschillende manieren om op te lijsten*/
+
+            dogAsiel[0] = new Dog("Xam", "protect");
+            dogAsiel[1] = new Dog("Pluto", "fun");
+            dogAsiel[2] = new Dog("Rob", "jump");
+            dogAsiel[3] = new Dog("Rob", "jump");
+            dogAsiel[4] = new Dog("Rob", "jump");
+
+        } catch (ArrayIndexOutOfBoundsException a) {
+            System.out.println(" Helaas er is geen plaats in het asiel");
+
+        }
+    }
+}
